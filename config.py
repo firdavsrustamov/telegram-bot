8483e18a1b0879621feedf3c488b4d056760a167
Firdavs Rustamov <74056031+firdavsrustamov@users.noreply.github.com>
2025-06-26 09:21:27 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 TOKEN = '7834040928:AAFu-3hVlaEj8cSkE04LslF3eAsXs4rk5Ho'
 
 # ID администратора (ваш Telegram ID)
-ADMIN_ID = 704550745  # Замените на ваш Telegram ID (например, 123456789)
+ADMIN_ID = 7971030441  # Замените на ваш Telegram ID (например, 123456789)
